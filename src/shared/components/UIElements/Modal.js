668f8e866a30fa20e3ea4d29934e98b28a5e0949
Adel-Abdallah <adelgafo@gmail.com>
2021-03-26 09:39:39 +0200

    modal ready!

---
+++ src/shared/components/UIElements/Modal.js
@@ -0,0 +1,40 @@
+import "./Modal.css";
+import { CSSTransition } from "react-transition-group";
+import Backdrop from "./Backdrop";
+
+const ModalOverlay = props => {
+  return (
+    <div className={`modal ${props.className}`} style={props.style}>
+      <header className={`modal__header ${props.headerClass}`}>
+        <h2>{props.header}</h2>
+      </header>
+      <form
+        onSubmit={props.onSubmit ? props.onSubmit : e => e.preventDefault()}>
+        <div className={`modal__content ${props.contentClass}`}>
+          {props.children}
+        </div>
+        <footer className={`modal__footer ${props.footerClass}`}>
+          {props.footer}
+        </footer>
+      </form>
+    </div>
+  );
+};
+
+const Modal = props => {
+  return (
+    <>
+      {props.show && <Backdrop onClick={props.onaCancel} />}
+      <CSSTransition
+        in={props.show}
+        mountOnEnter
+        unmountOnExit
+        timeout={200}
+        classNames='modal'>
+        <ModalOverlay {...props} />
+      </CSSTransition>
+    </>
+  );
+};
+
+export default Modal;
