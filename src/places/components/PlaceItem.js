0c3c560fc67b8afc7bc3471e49ff9530e0e8bef6
Adel-Abdallah <adelgafo@gmail.com>
2021-03-27 01:15:49 +0200

    OpenLayers Docs Examples API openLayers map added

--- src/places/components/PlaceItem.js
+++ src/places/components/PlaceItem.js
@@ -3,7 +3,7 @@ import React, { useState } from "react";
 import Card from "../../shared/components/UIElements/Card";
 import "./PlaceItem.css";
 import Button from "./../../shared/components/FormElements/Button";
-
+import Map from "../../shared/components/UIElements/Map";
 import Modal from "../../shared/components/UIElements/Modal";
 
 const PlaceItem = props => {
@@ -20,8 +20,26 @@ const PlaceItem = props => {
         contentClass='place-item__modal-content'
         footerClass='place-item__modal-actions'
         footer={<Button onClick={closeMapHandler}>CLOSE</Button>}>
-        <div className='map-container'>
-          <h2>THE MAP</h2>
+        <div className='map-container' style={{ padding: "5px" }}>
+          {/* <iframe
+            title='map'
+            width='100%'
+            height='100%'
+            frameBorder='0'
+            scrolling='no'
+            marginHeight='0'
+            marginWidth='0'
+            src={
+              "https://maps.google.com/maps?q=" +
+              props.coordinates.lat.toString() +
+              "," +
+              props.coordinates.lng.toString() +
+              "&t=&z=15&ie=UTF8&iwloc=&output=embed"
+            }></iframe>
+          <script
+            type='text/javascript'
+            src='https://embedmaps.com/google-maps-authorization/script.js?id=5a33be79e53caf0a07dfec499abf84b7b481f165'></script> */}
+          <Map center={props.coordinates} zoom={16} />
         </div>
       </Modal>
       <li className='place-item'>
